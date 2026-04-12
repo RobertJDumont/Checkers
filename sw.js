8d3ef51fcc9e6b7bcfca4506f68fa0ade9b8266e
Robert Dumont <robertjdumont@gmail.com>
2026-04-12 21:16:54 +0200

    chore: update service worker cache version and adjust asset paths to relative URLs

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
-const CACHE_NAME = 'checkers-cache-v3';
+const CACHE_NAME = 'checkers-cache-v4';
 const ASSETS_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/style.css',
-    '/script.js',
-    '/manifest.json',
-    '/icon.svg'
+    './',
+    './index.html',
+    './style.css',
+    './script.js',
+    './manifest.json',
+    './icon.svg'
 ];
 
 self.addEventListener('install', (event) => {
